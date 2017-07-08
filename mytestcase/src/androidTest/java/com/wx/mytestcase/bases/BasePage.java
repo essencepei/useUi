11d131e4cef5e4e078c2e18b2b3d43bad12bf549
essence <essencepei@gmail.com>
2017-07-08 14:58:33 +0800

    更改获取元素方法

--- mytestcase/src/androidTest/java/com/wx/mytestcase/bases/BasePage.java
+++ mytestcase/src/androidTest/java/com/wx/mytestcase/bases/BasePage.java
@@ -1,7 +1,12 @@
 package com.wx.mytestcase.bases;
 
 import android.support.test.InstrumentationRegistry;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.BySelector;
 import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject2;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/6/20 0020.
@@ -9,4 +14,26 @@ import android.support.test.uiautomator.UiDevice;
 
 public class BasePage {
     protected UiDevice mUIDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
+    public UiObject2 myFindObjectByText(String text){
+        return  mUIDevice.findObject(By.text(text));
+    }
+    public UiObject2 myFindObjectByDesc(String desc){
+        return  mUIDevice.findObject(By.desc(desc));
+    }
+    public UiObject2 myFindObjectById(String id){
+        return  mUIDevice.findObject(By.res(id));
+    }
+
+    public UiObject2 myFindObjectByClassName(String className){
+        return  mUIDevice.findObject(By.clazz(className));
+    }
+
+    public UiObject2 myFindObjectByClass(Class clazz){
+        return  mUIDevice.findObject(By.clazz(clazz));
+    }
+
+    public List<UiObject2> myFindObjects(BySelector by){
+        return  mUIDevice.findObjects(by);
+    }
 }
